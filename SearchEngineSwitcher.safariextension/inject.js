d175f7baa0e26159665e17d0433bfc8d4c43610f
Ray <ray@rayps.com>
2018-06-06 03:48:49 +0800

    Update main scripts

--- SearchEngineSwitcher.safariextension/inject.js
+++ SearchEngineSwitcher.safariextension/inject.js
@@ -1,15 +1,16 @@
 safari.self.addEventListener("message", switchSearchEngine, false);
 
 function switchSearchEngine (Event) {
-
-    var keyword = ""
-    var hostname = location.hostname
-
-    if (hostname.indexOf("baidu") < 0){
-        keyword = document.getElementsByName("q")[0].value
-        location.href = "http://www.baidu.com/s?wd=" + keyword
-    } else if (hostname.indexOf("google") < 0){
-        keyword = document.getElementsByName("wd")[0].value
-        location.href = "https://www.google.com/?&q=#q=" + keyword
+    if (Event.name == "switchSearchEngine") {
+        var keyword = ""
+        var hostname = location.hostname
+    
+        if (hostname.indexOf("baidu") < 0){
+            keyword = document.getElementsByName("q")[0].value
+            location.href = "http://www.baidu.com/s?wd=" + keyword
+        } else if (hostname.indexOf("google") < 0){
+            keyword = document.getElementsByName("wd")[0].value
+            location.href = "https://www.google.com/?&q=#q=" + keyword
+        }
     }
 }
