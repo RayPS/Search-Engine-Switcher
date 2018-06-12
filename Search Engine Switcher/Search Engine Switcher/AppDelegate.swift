fd372f7c566e2c6cc431e1f8400f9742c80012f6
Ray <ray@rayps.com>
2018-06-12 23:49:05 +0800

    Create Xcode project

---
+++ Search Engine Switcher/Search Engine Switcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Search Engine Switcher
+//
+//  Created by Ray on 2018/6/12.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
