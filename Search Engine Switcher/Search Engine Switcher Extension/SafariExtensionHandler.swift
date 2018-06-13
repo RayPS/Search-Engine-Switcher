4b448228a2ecf43ead301c424f2872b6c5bd1b2b
Ray <ray@rayps.com>
2018-06-13 20:25:27 +0800

    😃

--- Search Engine Switcher/Search Engine Switcher Extension/SafariExtensionHandler.swift
+++ Search Engine Switcher/Search Engine Switcher Extension/SafariExtensionHandler.swift
@@ -12,19 +12,78 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
         // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
-        page.getPropertiesWithCompletionHandler { properties in
-            NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
-        }
+//        page.getPropertiesWithCompletionHandler { props in
+//        }
     }
     
     override func toolbarItemClicked(in window: SFSafariWindow) {
         // This method will be called when your toolbar item is clicked.
-        NSLog("The extension's toolbar item was clicked")
+//        NSLog("The extension's toolbar item was clicked")
+        window.getActivePage{ (tab, page, props) in
+            let url = props?.url
+            let isGoogle = url?.host?.range(of: "google") != nil
+
+            let queryKey = isGoogle ? "q" : "wd"
+            let keyword = url?.queryParameters?[queryKey] ?? ""
+            let switchedUrlString = isGoogle ? "https://baidu.com/s?wd=\(keyword)" : "https://google.com/search?q=\(keyword)"
+            page?.dispatchMessageToScript(withName: "switch", userInfo: ["switchedUrlString": switchedUrlString])
+        }
     }
     
     override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
         // This is called when Safari's state changed in some way that would require the extension's toolbar item to be validated again.
-        validationHandler(true, "")
+        window.getActivePage{ (tab, page, props) in
+            let url = props?.url
+            let isGoogle = url?.host?.range(of: "google") != nil
+            let isBaidu = url?.host?.range(of: "baidu") != nil
+
+            if (isGoogle || isBaidu) {
+                let icon = isGoogle ? "toolbar-icon-baidu.pdf" : "toolbar-icon-google.pdf"
+                window.setToolbarItemImage(resourceName: icon)
+                validationHandler(true, "")
+            } else {
+                window.setToolbarItemImage(resourceName: "toolbar-icon.pdf")
+                validationHandler(false, "")
+            }
+        }
+
+
     }
 
+}
+
+extension SFSafariWindow {
+
+    func getActivePage(_ completion: @escaping ((SFSafariTab?, SFSafariPage?, SFSafariPageProperties?) -> Void)) {
+        self.getActiveTab { tab in
+            tab?.getActivePage(completionHandler: { page in
+                page?.getPropertiesWithCompletionHandler({ props in
+                    completion(tab, page, props)
+                })
+            })
+        }
+    }
+
+    func setToolbarItemImage(resourceName: String) {
+        self.getToolbarItem { (item) in
+            item?.setImage(NSImage(imageLiteralResourceName: resourceName))
+        }
+    }
+}
+
+
+extension URL {
+
+    public var queryParameters: [String: String]? {
+        guard let components = URLComponents(url: self, resolvingAgainstBaseURL: true), let queryItems = components.queryItems else {
+            return nil
+        }
+
+        var parameters = [String: String]()
+        for item in queryItems {
+            parameters[item.name] = item.value
+        }
+
+        return parameters
+    }
 }
--- Search Engine Switcher/Search Engine Switcher Extension/script.js
+++ Search Engine Switcher/Search Engine Switcher Extension/script.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
+document.addEventListener('DOMContentLoaded', function(event) {
+    // safari.extension.dispatchMessage("DOMContentLoaded");
+    safari.self.addEventListener('message', messageHandler, false);
+
+    function messageHandler (event) {
+        if (event.name == 'switch') {
+            location.href = event.message.switchedUrlString;
+        }
+    }
+
 });
